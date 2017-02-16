0d70f59dfc963fcce271fa47ed5453cdf2c80d8e
Zhiwei Huang <zhiweih.int@gmail.com>
2017-02-16 00:05:08 -0800

    Add serach bar for last_name and first_name for Author in admin site

--- booknotes/admin.py
+++ booknotes/admin.py
@@ -3,6 +3,7 @@ from .models import Author, Book, Publisher
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
 
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
