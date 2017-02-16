c083e34002c4d4ada358b11038dc2a9716bde245
Zhiwei Huang <zhiweih.int@gmail.com>
2017-02-16 10:27:40 -0800

    ModelAdmin class for Book

--- booknotes/admin.py
+++ booknotes/admin.py
@@ -5,6 +5,12 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
     search_fields = ('first_name', 'last_name')
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date',)
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)
+
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Publisher)
