ec2754c9d5035b66d1a21fad455a0da824179529
Zhiwei Huang <zhiweih.int@gmail.com>
2017-02-16 00:02:24 -0800

    Customize display name/list for Author in admin sites

--- booknotes/admin.py
+++ booknotes/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Author, Book, Publisher
 
-admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
 admin.site.register(Publisher)
